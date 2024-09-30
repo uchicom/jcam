28b3a1056668e2c8d8711683aa966ad7c20ec5b5
shigeki.uchiyama <shigeki.uchiyama@uchicom.com>
2024-10-01 06:16:15 +0900

    pom.xml追加 #1

--- src/main/java/com/uchicom/jcam/Main.java
+++ src/main/java/com/uchicom/jcam/Main.java
@@ -1,41 +1,54 @@
+// (C) 2024 Rigger LLC
 package com.uchicom.jcam;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
-
 import javax.imageio.ImageIO;
-
-import org.bytedeco.javacv.Frame;
-import org.bytedeco.javacv.Java2DFrameConverter;
-import org.bytedeco.javacv.OpenCVFrameGrabber;
+import org.jcodec.codecs.h264.BufferH264ES;
+import org.jcodec.codecs.h264.H264Decoder;
+import org.jcodec.common.io.NIOUtils;
+import org.jcodec.common.model.ColorSpace;
+import org.jcodec.common.model.Packet;
+import org.jcodec.common.model.Picture;
+import org.jcodec.scale.AWTUtil;
 
 public class Main {
 
-    public static void main(String[] args) {
-      
-        try (Java2DFrameConverter converter = new Java2DFrameConverter();
-        OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(0);){
-        // 開始
-        grabber.start();
-        for (int i = 0; i < 10; i++) {
-          // 画像取得
-          var image = grabber.grab();
+  public static void main(String[] args) {
 
-          // 画像表示
-          if (image != null) {
-            var bufferedImage = converter.getBufferedImage(image, 1.0D, false, null);
-            ImageIO.write(bufferedImage, "jpg", new File(System.currentTimeMillis() + ".jpg"));
+    try {
+      long startTime = System.currentTimeMillis();
+      File file = new File("test.h264");
+
+      var buf = NIOUtils.fetchFromFile(file);
+      try (var es = new BufferH264ES(buf)) {
+        var decoder = new H264Decoder();
+        Picture pic = Picture.create(1920, 1088, ColorSpace.YUV420);
+
+        var i = 0;
+        Packet nextFrame = es.nextFrame();
+        var start = nextFrame.data.position();
+        var frame = decoder.decodeFrame(nextFrame.data, pic.getData()).cropped();
+        var length = nextFrame.data.position() - start;
+        System.out.println(length);
+        while ((nextFrame = es.nextFrame()) != null) {
+          start = nextFrame.data.position();
+          frame = decoder.decodeFrame(nextFrame.data, pic.getData());
+          length = nextFrame.data.position() - start;
+          System.out.println(length);
+          if (length > 10000 && !nextFrame.isKeyFrame()) {
+            BufferedImage bufferedImage = AWTUtil.toBufferedImage(frame);
+            System.out.println(bufferedImage.getWidth() + "x" + bufferedImage.getHeight());
+            ImageIO.write(bufferedImage, "png", new File("frame" + i + ".png"));
           }
+          i++;
         }
-        
-        try {
-          // 終了
-          grabber.stop();
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
-      } catch (Exception e) {
-        e.printStackTrace();
+        System.out.println("frame count:" + i);
+        System.out.println("time:" + (System.currentTimeMillis() - startTime) / 1000 + "s");
       }
 
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 }
